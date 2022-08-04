c9be43770f2f8905beb77e80fcb51f563a87a088
ce8erg0 <1ce8erg0@users.noreply.github.com>
2022-08-04 14:19:54 +1000

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='left_pad',
+    version='',
+    packages=['left_pad'],
+    url='',
+    license='',
+    author='ce8erg0',
+    author_email='',
+    description=''
+)
